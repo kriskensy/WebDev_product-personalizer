c9d5787eebdd68523c66f5750ab870edabdac277
kris.kensy <kriskensy@proton.me>
2025-06-13 22:29:03 +0200

    Add propTypes to Product component. Add useStates for color, size, name

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,29 +1,42 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes, { object, string } from 'prop-types';
+import { useState } from 'react';
+import productsData from '../../data/products';
 
 const Product = props => {
+
+  const [currentName, setCurrentName] = useState(productsData[0].name);
+  const [currentColor, setCurrentColor] = useState(productsData[0].colors[0]);
+  const [currentSize, setCurrentSize] = useState(productsData[0].sizes[0].name);
+
+//TODO wywalic console.logi
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={productsData.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-` + currentName + `--` + currentColor + `.jpg`} />
+
+          {console.log('name: ', currentName)};
+          {console.log('color: ', currentColor)};
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>{props.basePrice} €</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              <li><Button>S</Button></li>
+              <li><Button>M</Button></li>
+              <li><Button>L</Button></li>
+              <li><Button className={styles.active}>XL</Button></li>
             </ul>
           </div>
           <div className={styles.colors}>
@@ -41,6 +54,15 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  title: PropTypes.string,
+  basePrice: PropTypes.number,
+  colors: PropTypes.arrayOf(string),
+  sizes: PropTypes.arrayOf(object)
 };
 
 export default Product;
